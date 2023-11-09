68307eb6ade52dd82e9d930f3f5d162f537cac5d
chamsabd <chamsabdelwahed42@gmail.com>
2023-11-09 11:18:17 +0100

    first commit

---
+++ fontend/src/app/shared/menu-items/menu-items.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+export interface Menu {
+  state: string;
+  name: string;
+  type: string;
+  icon: string;
+}
+
+const MENUITEMS = [
+  { state: 'dashboard', name: 'Dashboard', type: 'link', icon: 'av_timer' },
+  { state: 'button', type: 'link', name: 'prof', icon: 'crop_7_5' },
+  { state: 'grid', type: 'link', name: 'etudiant', icon: 'view_comfy' },
+  { state: 'lists', type: 'link', name: 'departement', icon: 'view_list' },
+  { state: 'menu', type: 'link', name: 'cours', icon: 'view_headline' },
+  { state: 'tabs', type: 'link', name: 'clubs', icon: 'tab' },
+  { state: 'stepper', type: 'link', name: 'salle', icon: 'web' },
+  // {
+  //   state: 'expansion',
+  //   type: 'link',
+  //   name: 'Expansion Panel',
+  //   icon: 'vertical_align_center'
+  // },
+  // { state: 'chips', type: 'link', name: 'Chips', icon: 'vignette' },
+  // { state: 'toolbar', type: 'link', name: 'Toolbar', icon: 'voicemail' },
+  // {
+  //   state: 'progress-snipper',
+  //   type: 'link',
+  //   name: 'Progress snipper',
+  //   icon: 'border_horizontal'
+  // },
+  // {
+  //   state: 'progress',
+  //   type: 'link',
+  //   name: 'Progress Bar',
+  //   icon: 'blur_circular'
+  // },
+  // {
+  //   state: 'dialog',
+  //   type: 'link',
+  //   name: 'Dialog',
+  //   icon: 'assignment_turned_in'
+  // },
+  // { state: 'tooltip', type: 'link', name: 'Tooltip', icon: 'assistant' },
+  // { state: 'snackbar', type: 'link', name: 'Snackbar', icon: 'adb' },
+  // { state: 'slider', type: 'link', name: 'Slider', icon: 'developer_mode' },
+  // {
+  //   state: 'slide-toggle',
+  //   type: 'link',
+  //   name: 'Slide Toggle',
+  //   icon: 'all_inclusive'
+  // }
+];
+
+@Injectable()
+export class MenuItems {
+  getMenuitem(): Menu[] {
+    return MENUITEMS;
+  }
+}
